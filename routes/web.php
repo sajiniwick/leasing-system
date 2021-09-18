ae79557e07141e6c0fe45a4a0f2f58603e8f89a3
sajiniwick <sajiniwick1@gmail.com>
2021-09-18 17:12:57 +0530

    login and dashboard UI

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,8 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\HomeController;
+use App\Http\Controllers\AdminDashboardController;
 
 /*
 |--------------------------------------------------------------------------
@@ -13,6 +15,5 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/', [HomeController::class, 'index']);
+Route::get('admin-dashboard', [AdminDashboardController::class, 'index']);
