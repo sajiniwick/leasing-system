6739f9979f8d176f97297d2e7f56402e557c9e76
sajiniwick <sajiniwick1@gmail.com>
2021-11-04 16:50:32 +0530

    navigation items added

---
+++ resources/views/admin/tenant/register.blade.php
@@ -0,0 +1,48 @@
+@extends('admin.layout.master')
+
+@section('content')
+
+    <!-- Content Header (Page header) -->
+    <div class="content-header">
+      <div class="container-fluid">
+        <div class="row mb-2">
+          <div class="col-sm-6">
+            <h1 class="m-0">Register Tenant</h1>
+          </div><!-- /.col -->
+          <div class="col-sm-6">
+            <ol class="breadcrumb float-sm-right">
+              <li class="breadcrumb-item"><a href="#">Home</a></li>
+              <li class="breadcrumb-item active">Tenants</li>
+            </ol>
+          </div><!-- /.col -->
+        </div><!-- /.row -->
+      </div><!-- /.container-fluid -->
+    </div>
+    <!-- /.content-header -->
+
+    <!-- Main content -->
+    <section class="content">
+      <div class="container-fluid">
+        
+      <form>
+        <div class="form-group">
+          <label for="fname">First Name</label>
+          <input type="text" class="form-control" id="fname" name="fname" aria-describedby="emailHelp" placeholder="Enter email">
+        </div>
+        <div class="form-group">
+          <label for="exampleInputEmail1">Email address</label>
+          <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Enter email">
+        </div>
+        <div class="form-group">
+          <label for="exampleInputPassword1">Password</label>
+          <input type="password" class="form-control" id="exampleInputPassword1" placeholder="Password">
+        </div>
+
+        <button type="submit" class="btn btn-primary">Submit</button>
+      </form>
+
+      </div><!-- /.container-fluid -->
+    </section>
+    <!-- /.content -->
+@endsection
+  
--- routes/web.php
+++ routes/web.php
@@ -4,6 +4,7 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\HomeController;
 use App\Http\Controllers\AdminDashboardController;
 use App\Http\Controllers\LoginController;
+use App\Http\Controllers\TenantController;
 
 //Home
 Route::get('/', [HomeController::class, 'index']);
@@ -12,3 +13,6 @@ Route::get('/', [HomeController::class, 'index']);
 Route::get('admin-dashboard', [LoginController::class, 'adminDashboard']);
 Route::post('user-login', [LoginController::class, 'userLogin']);
 Route::get('user-logout', [LoginController::class, 'userLogout']);
+
+//Tenant Module
+Route::resource('admin-tenants', TenantController::class);
